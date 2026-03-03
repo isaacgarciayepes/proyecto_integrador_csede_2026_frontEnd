d73873042c359ce85c3ebc13fd1e00e52b8d2d0f
kriztinajg-jpg <kriztinajg@gmail>
2026-03-02 21:19:28 -0500

    creacion de la logica

--- script.js
+++ script.js
@@ -9,3 +9,36 @@ const loginScreen = document.getElementById('login-screen');
 const dashboardScreen = document.getElementById('dashboard-screen');
 const alertBox = document.getElementById('alert-box');
 const alertText = document.getElementById('alert-text');
+/*
+Aqui logica, creamos una funcion y bucles
+*/
+function validarAcceso() {L
+    const emailValue = document.getElementById('email').value;
+    const passValue = document.getElementById('password').value;
+// con el value tomamos la informacion que ingrese el usuario
+    console.log("--- Intento de Ingreso ---");
+    console.log("Usuario: " + emailValue);
+    console.log("Password: " + passValue);
+
+    if (emailValue === USUARIO_VALIDO && passValue === CLAVE_VALIDA) {
+        console.log("Acceso exitoso.");
+        loginScreen.classList.add('hidden');
+        dashboardScreen.classList.remove('hidden');
+    } else {
+        intentos++;
+        console.warn("Intento fallido #" + intentos);
+
+        if (intentos >= MAX_INTENTOS) {
+            alertBox.classList.remove('hidden');
+            alertText.innerHTML = "<strong>¡Atención!</strong><br>Demasiados intentos de acceso.<br>Por favor, espere unos minutos e intente de nuevo.";
+            
+            btnIngresar.disabled = true;
+            btnIngresar.style.opacity = "0.5";
+            btnIngresar.innerText = "BLOQUEADO";
+        } else {
+            alert("Credenciales incorrectas. Intentos restantes: " + (MAX_INTENTOS - intentos));
+        }
+    }
+}
+// aqui iniciamos, al uso de bton ingresar
+btnIngresar.onclick = validarAcceso;

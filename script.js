7be0c1e84a96a11d8a52de01dbc66db8096c373d
kriztinajg-jpg <kriztinajg@gmail>
2026-03-02 21:02:15 -0500

    Creacion de variables y union con el html

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+//Definicion de variables 
+const USUARIO_VALIDO = "usuario@Idep.edu";
+const CLAVE_VALIDA = "123456";
+let intentos = 0;
+const MAX_INTENTOS = 3;
+// Union con el html, esto es para leer lo que tenemos en html 
+const btnIngresar = document.getElementById('btn-ingresar');
+const loginScreen = document.getElementById('login-screen');
+const dashboardScreen = document.getElementById('dashboard-screen');
+const alertBox = document.getElementById('alert-box');
+const alertText = document.getElementById('alert-text');
